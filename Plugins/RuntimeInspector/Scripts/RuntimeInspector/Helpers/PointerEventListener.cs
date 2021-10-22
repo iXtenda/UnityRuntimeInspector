8fa7baf29fd5823c61b70b4d8d1348754e123fe7
David Kutschke <david.kutschke@i-xt.com>
2021-10-22 16:36:36 +0200

    Added options to PointereventListener to filter mouse buttons

--- Plugins/RuntimeInspector/Scripts/RuntimeInspector/Helpers/PointerEventListener.cs
+++ Plugins/RuntimeInspector/Scripts/RuntimeInspector/Helpers/PointerEventListener.cs
@@ -8,22 +8,30 @@ namespace RuntimeInspectorNamespace
 		public delegate void PointerEvent( PointerEventData eventData );
 
 		public event PointerEvent PointerDown, PointerUp, PointerClick;
+		public bool reactToLeftClick = true;
+		public bool reactToRightClick = true;
+		public bool reactToMiddleClick = true;
+
+		bool IsReactingToButton( PointerEventData eventData )
+			=> eventData.button == PointerEventData.InputButton.Left   && reactToLeftClick
+			|| eventData.button == PointerEventData.InputButton.Right  && reactToRightClick
+			|| eventData.button == PointerEventData.InputButton.Middle && reactToMiddleClick;
 
 		void IPointerDownHandler.OnPointerDown( PointerEventData eventData )
 		{
-			if( PointerDown != null )
+			if( PointerDown != null && IsReactingToButton( eventData ) )
 				PointerDown( eventData );
 		}
 
 		void IPointerUpHandler.OnPointerUp( PointerEventData eventData )
 		{
-			if( PointerUp != null )
+			if( PointerDown != null && IsReactingToButton( eventData ) )
 				PointerUp( eventData );
 		}
 
 		void IPointerClickHandler.OnPointerClick( PointerEventData eventData )
 		{
-			if( PointerClick != null )
+			if( PointerClick != null && IsReactingToButton( eventData ) )
 				PointerClick( eventData );
 		}
 	}
